d20f748514e327e1a882118f6560210dc4fe37fc
chrisbeard <christopher.e.beard@gmail.com>
2015-09-19 12:59:24 -0400

    Collisions with enemies, mixtapes, fire emoji

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 import pygame
 import math
 import os
+import random
 from pygame.locals import *
 
 ## Util functions
@@ -29,30 +30,63 @@ def load_wav(name):
 
 
 ## Objects
+class enemy_obj(pygame.sprite.Sprite):
+
+    move_dist = 12
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        # self.x = x
+        # self.y = y
+
+        # info about image
+        self.image, self.rect = load_png('hater.png')
+        self.fire, self.fire_rect = load_png('fire.png')
+        self.height = self.rect.height
+        self.width = self.rect.width
+        self.x = x
+        self.y = y
+        self.rect.left = x
+        self.rect.top = y
+        self.on_fire = -1
+
+    def move(self):
+        if self.on_fire is -1:
+            self.x -= self.move_dist
+            self.rect.x -= self.move_dist
+
+
+    def draw(self, surface):
+        # surface.blit(self.image, (self.x, self.y))
+        surface.blit(self.image, self.rect)
+        if self.on_fire != -1:
+            surface.blit(self.fire, self.rect)
+
 class mixtape_obj(pygame.sprite.Sprite):
 
     move_dist = 15
 
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
 
         # info about image
         self.image, self.rect = load_png('mixtape.png')
+        self.rect.left = x
+        self.rect.top = y
         self.height = self.rect.height
         self.width = self.rect.width
+        self.consumed = False
 
         #sound effect
         self.superhot = pygame.mixer.Sound(load_wav('superhot.wav'))
 
     def move(self):
-        self.x += self.move_dist
+        self.rect.left += self.move_dist
         self.superhot.play(loops=0, maxtime=0) #play sound effect
         self.superhot.stop()
 
     def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
+        surface.blit(self.image, self.rect)
 
 class player_obj(pygame.sprite.Sprite):
 
@@ -60,13 +94,15 @@ class player_obj(pygame.sprite.Sprite):
 
     def __init__(self, x, y, level=1):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
 
         # info about image
         self.image, self.rect = load_png('player.png')
         self.height = self.rect.height
         self.width = self.rect.width
+        self.x = x
+        self.y = y
+        self.rect.left = x
+        self.rect.top = y
 
 
         # info about environment
@@ -80,34 +116,42 @@ class player_obj(pygame.sprite.Sprite):
         self.mixtapes_remaining = level * 12
 
 
-    def moveup(self):
+    def move_up(self):
         if self.y >= self.move_dist:
+            self.rect.y -= self.move_dist
             self.y -= self.move_dist
         else:
+            self.rect.y = 0
             self.y = 0
 
-    def movedown(self):
+    def move_down(self):
         if self.y <= self.max_height - (self.height + self.move_dist):
+            self.rect.y += self.move_dist
             self.y += self.move_dist
         else:
+            self.rect.y = self.max_height - self.height
             self.y = self.max_height - self.height
 
-    def moveright(self):
-        if self.x <= self.max_width/2:
+    def move_right(self):
+        if self.x <= 3*self.max_width/5:
+            self.rect.x += self.move_dist
             self.x += self.move_dist
 
-    def moveleft(self):
+    def move_left(self):
         if self.x >= self.move_dist:
+            self.rect.x -= self.move_dist
             self.x -= self.move_dist
 
-    def movemixtapes(self):
+    def move_mixtapes(self):
         for tape in self.mixtapes:
             tape.move()
+        mixtapes = [m for m in self.mixtapes if m.rect.x < self.max_width]
 
     def draw(self, surface):
-        surface.blit(self.image, (self.x, self.y))
+        # surface.blit(self.image, (self.x, self.y))
+        surface.blit(self.image, self.rect)
         for tape in self.mixtapes:
-            surface.blit(tape.image, (tape.x, tape.y))
+            tape.draw(surface)
 
     def fire(self):
         if self.mixtapes_remaining > 0:
@@ -115,6 +159,17 @@ class player_obj(pygame.sprite.Sprite):
             self.mixtapes.append(b)
             self.mixtapes_remaining -= 1
 
+    def check_collisions(self, enemies):
+        alive_enemies = [e for e in enemies if e.on_fire is -1]
+        for tape in self.mixtapes:
+            for e in alive_enemies:
+                if tape.rect.colliderect(e.rect):
+                    e.on_fire = 5
+                    print "Hit!"
+                    tape.consumed = True
+        self.mixtapes = [m for m in self.mixtapes if not m.consumed]
+        alive_enemies = [e for e in alive_enemies if e.on_fire is -1]
+        return self.rect.collidelist(alive_enemies) != -1
 
 
 ## Running the game
@@ -122,13 +177,17 @@ class player_obj(pygame.sprite.Sprite):
 def main():
 
     # misc data
-    _height = 600
-    _width = 900
+    _height = 720
+    _width = 1360
     _player_height = 92
+    _level = 1
 
     # Initialise screen
     pygame.init()
-    screen = pygame.display.set_mode((_width, _height))
+    # screen = pygame.display.set_mode((_width, _height))
+    # screen = pygame.display.set_mode(resolution=(0,0), flags=pygame.FULLSCREEN, depth=0)
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN, 0)
+    print screen.get_size()
     pygame.display.set_caption('StraightFire')
 
     # Fill background
@@ -137,7 +196,7 @@ def main():
     background.fill((250, 250, 250))
 
     # Initialize player
-    player = player_obj(0, _height - _player_height)
+    player = player_obj(0, _height - _player_height, _level)
 
     # Initialize sprites
     playersprite = pygame.sprite.RenderPlain((player))
@@ -155,10 +214,13 @@ def main():
     movedown = False
     moveright = False
     moveleft = False
+    pygame.time.set_timer(USEREVENT+1, 1500)
+
+    haters = []
 
     # Event loop
     while 1:
-        clock.tick(40) #cap at 60fps
+        clock.tick(60) #cap at 60fps
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -183,13 +245,22 @@ def main():
                     moveright = False
                 if event.key == K_LEFT:
                     moveleft = False
+            elif event.type == USEREVENT+1:
+                enemy = enemy_obj(_width, random.randrange(0,_height-_player_height,15))
+                haters.append(enemy)
 
 
-        player.moveup() if moveup else None
-        player.movedown() if movedown else None
-        player.moveright() if moveright else None
-        player.moveleft() if moveleft else None
-        player.movemixtapes()
+        player.move_up() if moveup else None
+        player.move_down() if movedown else None
+        player.move_right() if moveright else None
+        player.move_left() if moveleft else None
+        player.move_mixtapes()
+        haters = [h for h in haters if h.x > -_player_height]
+        for h in haters:
+            h.move()
+        if player.check_collisions(haters):
+            print "Collision"
+            break
 
         # prepare screen to be re-drawn
         screen.blit(background, (0, 0))
@@ -198,7 +269,7 @@ def main():
         # mixtapes remaining
         mixtapes_left = (10,10,10)
         no_mixtapes = (250,0,0)
-        text = font.render("Mixtapes: {}".format(player.mixtapes_remaining), 1,
+        text = font.render("Level: {}   Mixtapes: {}".format(_level, player.mixtapes_remaining), 1,
                 mixtapes_left if player.mixtapes_remaining > 0 else no_mixtapes)
         textpos = text.get_rect()
         textpos.centerx = background.get_rect().centerx
@@ -206,7 +277,11 @@ def main():
 
         # playersprite.update()
         player.draw(screen)
+        for h in haters:
+            h.draw(screen)
         pygame.display.flip()
+
+    print "You lose"
 
 if __name__ == '__main__': main()
 
